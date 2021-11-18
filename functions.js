92f6975657cc1916de6a3f32246ebf00ab3a74ff
sanjarbek <88580441+sanjarDev1@users.noreply.github.com>
2021-11-18 23:28:32 +0500

    Add files via upload

--- functions.js
+++ functions.js
@@ -14,3 +14,26 @@ const circle1 = createCircle(2);
 const circle2 = createCircle(3);
 // console.log(circle2);
 console.log(circle1.constructor);
+
+
+
+//  constructor function
+
+//  BuPascalCase
+//  buCamelCase
+
+function Circle(radius) {
+    this.raduis = radius;
+    this.draw = function () {
+        console.log('doira');
+    }
+}
+
+const circle = new Circle(1);
+circle.color = 'green';
+circle.move = function () {
+
+}
+delete circle.color;
+console.log(circle.constructor);
+
